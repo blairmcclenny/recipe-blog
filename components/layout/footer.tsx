40467ab3bcec9085848459bdaeaee62f2470e643
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-09-27 11:42:19 -0700

    footer wip

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,11 @@
+import Copyright from "./copyright"
+
 export default function Footer() {
   return (
     <footer>
       <div className="container mx-auto px-4 py-8">
         <p className="text-center text-sm text-gray-400">
-          &copy; {new Date().getFullYear()} Sticky Footer
+          <Copyright /> Lorem Ipsum
         </p>
       </div>
     </footer>
