bf1ae381d530b39b8f9afd993e59f3d45179a654
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-10-21 12:40:28 -0700

    add page queries

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import renderRichText from "@/components/richText"
 import { TypographyH1 } from "@/components/typography"
+import { getPages } from "@/lib/queries/pages"
 
-export default function Home() {
+export default async function Home() {
+  const data = await getPages({ pageSlug: "home", isDraftMode: false })
+  const page = data?.pageCollection?.items?.[0]
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TypographyH1>Home Page</TypographyH1>
+      <TypographyH1>{page.title}</TypographyH1>
+      <div className="prose lg:prose-lg">
+        {renderRichText(page.details.json)}
+      </div>
     </main>
   )
 }
