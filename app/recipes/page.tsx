05166e4f3683743d575c134ad0e752678c333f58
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-10-23 11:21:14 -0700

    add basic container to all pages

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation"
 import { draftMode } from "next/headers"
 import { Card, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
+import { TypographyH1 } from "@/components/typography"
 
 export default async function Page() {
   const { isEnabled } = draftMode()
@@ -18,16 +19,19 @@ export default async function Page() {
   }
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {recipes.map((recipe) => (
-        <Link href={`/recipes/${recipe.slug}`} key={recipe.sys.id}>
-          <Card>
-            <CardHeader>
-              <CardTitle>{recipe.title}</CardTitle>
-            </CardHeader>
-          </Card>
-        </Link>
-      ))}
+    <div className="container mx-auto px-4">
+      <TypographyH1>Recipes</TypographyH1>
+      <div className="grid grid-cols-4 gap-4">
+        {recipes.map((recipe) => (
+          <Link href={`/recipes/${recipe.slug}`} key={recipe.sys.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{recipe.title}</CardTitle>
+              </CardHeader>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
