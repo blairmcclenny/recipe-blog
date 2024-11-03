4e233d3f1473079ec6da6ea2e4900b374307bb3c
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-11-03 13:08:33 -0800

    use function for rich text options

--- components/richText.tsx
+++ components/richText.tsx
@@ -1,7 +1,7 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import { BLOCKS } from "@contentful/rich-text-types"
+import { BLOCKS, Node } from "@contentful/rich-text-types"
 import {
-  TypographyBlockquote,
+  // TypographyBlockquote,
   TypographyH1,
   TypographyH2,
   TypographyH3,
@@ -16,44 +16,48 @@ import {
 // TODO
 // Audit and update types (richText, options)
 
-const defaultOptions = {
-  renderMark: {},
-  renderNode: {
-    [BLOCKS.HEADING_1]: (
-      node: React.ReactNode,
-      children: React.ReactElement
-    ) => <TypographyH1>{children}</TypographyH1>,
-    [BLOCKS.HEADING_2]: (
-      node: React.ReactNode,
-      children: React.ReactElement
-    ) => <TypographyH2>{children}</TypographyH2>,
-    [BLOCKS.HEADING_3]: (
-      node: React.ReactNode,
-      children: React.ReactElement
-    ) => <TypographyH3>{children}</TypographyH3>,
-    [BLOCKS.HEADING_4]: (
-      node: React.ReactNode,
-      children: React.ReactElement
-    ) => <TypographyH4>{children}</TypographyH4>,
-    [BLOCKS.PARAGRAPH]: (
-      node: React.ReactNode,
-      children: React.ReactElement
-    ) => <TypographyP>{children}</TypographyP>,
-    [BLOCKS.UL_LIST]: (node: React.ReactNode, children: React.ReactElement) => (
-      <TypographyUL>{children}</TypographyUL>
-    ),
-    [BLOCKS.OL_LIST]: (node: React.ReactNode, children: React.ReactElement) => (
-      <TypographyOL>{children}</TypographyOL>
-    ),
-    [BLOCKS.LIST_ITEM]: (
-      node: React.ReactNode,
-      children: React.ReactElement
-    ) => <TypographyLI>{children}</TypographyLI>,
-    [BLOCKS.HR]: () => <TypographyHR />,
-    [BLOCKS.QUOTE]: (node: React.ReactNode, children: React.ReactElement) => (
-      <TypographyBlockquote>{children}</TypographyBlockquote>
-    ),
-  },
+const defaultOptions = () => {
+  return {
+    renderMark: {},
+    renderNode: {
+      [BLOCKS.HEADING_1]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyH1>{children}</TypographyH1>,
+      [BLOCKS.HEADING_2]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyH2>{children}</TypographyH2>,
+      [BLOCKS.HEADING_3]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyH3>{children}</TypographyH3>,
+      [BLOCKS.HEADING_4]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyH4>{children}</TypographyH4>,
+      [BLOCKS.PARAGRAPH]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyP>{children}</TypographyP>,
+      [BLOCKS.UL_LIST]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyUL>{children}</TypographyUL>,
+      [BLOCKS.OL_LIST]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyOL>{children}</TypographyOL>,
+      [BLOCKS.LIST_ITEM]: (
+        node: Node,
+        children: React.ReactNode
+      ) => <TypographyLI>{children}</TypographyLI>,
+      [BLOCKS.HR]: () => <TypographyHR />,
+      // [BLOCKS.QUOTE]: (node: React.ReactNode, children: React.ReactElement) => (
+      //   <TypographyBlockquote>{children}</TypographyBlockquote>
+      // ),
+    },
+  }
 }
 
 export default function renderRichText(
@@ -62,5 +66,5 @@ export default function renderRichText(
 ) {
   if (!richText) return null
 
-  return documentToReactComponents(richText, options)
+  return documentToReactComponents(richText, options())
 }
