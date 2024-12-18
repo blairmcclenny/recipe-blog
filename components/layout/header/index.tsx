122dec0c93069892930a94fb787b6b7a46da70a3
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-12-18 11:29:04 -0800

    rename layout header files

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,5 +1,5 @@
 import { getNavigation } from "@/lib/queries/navigation"
-import HeaderNav from "@/components/layout/header-nav"
+import HeaderNav from "@/components/layout/header/nav"
 
 export default async function Header() {
   const data = await getNavigation({ title: "Header", isDraftMode: false })
